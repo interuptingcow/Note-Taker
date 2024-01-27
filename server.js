1c5096550df97dd6fb4e23ec732b86d303d1aacb
interuptingcow <npad28@gmail.com>
2024-01-27 15:03:34 -0800

    completed get and post functionality and fixed path issues

--- server.js
+++ server.js
@@ -2,19 +2,46 @@ const express = require("express");
 const path = require('path');
 const PORT = 3001;
 const app = express();
+const fs = require('fs');
+const uuid = require('uuid');
 
-app.use(express.static('public'));
+app.use(express.static('develop/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/develop/public/index.html'));
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
-  });
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+});
+
+const showNotes = () => {
+  const notes = fs.readFileSync(path.join(__dirname, "Develop/db/db.json"))
+  return JSON.parse(notes);
+}
+
+const saveNote = (notes) => {
+  fs.writeFileSync(path.join(__dirname, "Develop/db/db.json"), JSON.stringify(notes));
+}
+app.get("/api/notes", (req, res) => {
+    let notes = showNotes();
+    res.json(notes);
+})
+
+app.post("/api/notes", (req, res) => {
+  const newNote = {...req.body, id:uuid.v4()};
+  let notes = showNotes();
+  notes.push(newNote);
+  saveNote(notes);
+  res.json(notes);
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+});
